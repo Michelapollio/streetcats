63562faf3e96e9089bfa51d53391884043c4cfd9
michela <polliomichela360@gmail.com>
2026-04-21 23:24:56 +0200

    refactor: remove unused backend files and models

--- backend/src/routes/catRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { createCat } from '../controllers/catController.js';
-import { upload } from '../middlewares/upload.js';
-const router = Router();
-router.post('/', upload.single('photo'), // il campo del file deve chiamarsi "photo"
-createCat);
-export default router;
